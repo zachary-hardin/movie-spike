8b6c6a10ee76099a100a7462ab7b35ef190e99c1
Zachary Hardin <zhardin@pillartechnology.com>
2018-03-19 16:26:48 -0500

    Fix.

--- test.js
+++ test.js
@@ -1,15 +1,30 @@
 const imdb = require('imdb-api');
-    
+
+let movie = {
+  title: ""
+};
 const apiKey = 'fa95c527';
 const auth = {
-    apiKey: apiKey,
-    timeout: 30000
+  apiKey: apiKey,
+  timeout: 30000
 };
 
 function getMovieById(title) {
-    return imdb.get(title, auth).then(response => {
-        return response;
-    });
+  return imdb.get(title, auth).then(response => {
+    return response;
+  });
 }
 
-console.log(Promise.resolve(getMovieById("The Walking Dead")));
+// console.log(Promise.resolve(getMovieById("The Walking Dead")));
+
+getMovieById("The Walking Dead").then((data) => {
+  movie = data;
+  console.log("foo");
+  console.log(movie);
+  processMovies();
+});
+
+function processMovies() {
+  console.log("bar");
+  console.log(movie);
+}
